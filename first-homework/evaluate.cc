d089a502abb8d73c376c2313bedc122c82681621
Vass Mark <Vass Mark>
2020-03-06 14:14:38 +0100

    fix to c++98

--- first-homework/evaluate.cc
+++ first-homework/evaluate.cc
@@ -8,26 +8,34 @@
 class FlexLexerHandler {
 private:
     std::ifstream input_;
-    yyFlexLexer flexer_;
+    yyFlexLexer* flexer_;
 
 public:
-    FlexLexerHandler(std::string&& path) {
+    FlexLexerHandler(char path[]) {
         input_.open(path);
         if(!input_) {
-            std::printf("%s file can not be opened!", path.c_str());  
+            std::fprintf(stderr, "%s file can not be opened!", path);  
             std::exit(-1);          
         }
 
-        flexer_ = yyFlexLexer(&input_, &std::cout);
+        flexer_ = new yyFlexLexer(&input_, &std::cout);
     }
 
     void evaluate() {
-        flexer_.yylex();
+        flexer_->yylex();
+    }
+
+    ~FlexLexerHandler() {
+        delete flexer_;
     }
 };
 
 
 int main(int argc, char* argv[]) {
-    auto flexer = FlexLexerHandler(std::move(argv[1]));
+    if(argc < 2) {
+        std::exit(-1);
+    }
+
+    FlexLexerHandler flexer(argv[1]);
     flexer.evaluate();
 }
